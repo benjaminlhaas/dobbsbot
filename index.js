c1597b9ae243355194e9383f9480909e3b6ff01d
Benjamin Haas <benjamin.haas@intersection.com>
2016-06-29 15:49:48 -0400

    add s3 read/write

--- index.js
+++ index.js
@@ -1,20 +1,58 @@
-var Twitter = require('twitter');
-var credentials = require("./credentials");
-var client = new Twitter(credentials);
+var readline = require('readline');
 
-var sendTweet = function() {
-	client.post('statuses/update', {status: 'test tweet ' + Math.random()}, function (error, tweet, response) {
-		if (error != null) {
-			console.log(error);
+var AWS = require('aws-sdk');
+//AWS.config.update({region: 'us-east-1'});
+var s3 = new AWS.S3();
+
+var s3_params = require("./s3_params");
+var params = {Bucket: s3_params.bucket, Key: s3_params.key};
+
+var rl = readline.createInterface({
+    input: s3.getObject(params).createReadStream()
+});
+
+var output = "";
+var firstLine = true;
+var lineToTweet = "";
+
+rl.on('line', function(line) {
+	if (firstLine == true) {
+
+	    firstLine = false;
+
+		var Twitter = require('twitter');
+		var credentials = require("./credentials");
+		var client = new Twitter(credentials);
+
+		client.post('statuses/update', {status: line}, function (error, tweet, response) {
+			if (error != null) {
+				console.log(error);
+			}
+		});
+
+	}
+	else
+		output += line + '\n';
+})
+.on('close', function() {
+	output += lineToTweet;
+	//console.log(output);
+
+	var params_new = {Bucket: s3_params.bucket, Key: s3_params.key, Body: output, ContentType: 'text/plain'};
+
+	s3.upload(params_new, function(err, data) {
+		if (err) {
+			console.log(err);
+			//context.fail("There was an error.");
+		} 
+		else {
+			console.log(data.ETag);
+			//context.succeed(data.ContentType);
 		}
 	});
-};
+});
 
-// client.get('statuses/show/', {id: '747855945355694084'}, function (error, data, response) {
-// 	console.log(error);
-// 	console.log(data);
-// });
 
-exports.handler = function (event, context) {
-	sendTweet(context.succeed, context.fail)
-};
+// exports.handler = function (event, context) {
+// 	sendTweet(context.succeed, context.fail)
+// };

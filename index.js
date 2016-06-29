eae7a0152112aa1757a9820a81a041d7e5d721c3
Benjamin Haas <benjamin.haas@intersection.com>
2016-06-29 16:24:55 -0400

    prepare for Lambda

--- index.js
+++ index.js
@@ -1,58 +1,63 @@
 var readline = require('readline');
-
 var AWS = require('aws-sdk');
-//AWS.config.update({region: 'us-east-1'});
-var s3 = new AWS.S3();
+var s3_params = require("s3_params");
 
-var s3_params = require("./s3_params");
-var params = {Bucket: s3_params.bucket, Key: s3_params.key};
+function sendTweet() {
+	// Get file from S3 and feed it into the ReadLine interface
+	var s3 = new AWS.S3();
+	var params = {Bucket: s3_params.bucket, Key: s3_params.key};
 
-var rl = readline.createInterface({
-    input: s3.getObject(params).createReadStream()
-});
+	var rl = readline.createInterface({
+	    input: s3.getObject(params).createReadStream()
+	});
 
-var output = "";
-var firstLine = true;
-var lineToTweet = "";
+	var output = "";
+	var firstLine = true;
+	var lineToTweet = "";
 
-rl.on('line', function(line) {
-	if (firstLine == true) {
+	// ReadLine interface receives "line" event each time a line is read, and "close" event when the file is closed
+	rl.on('line', function(line) {
 
-	    firstLine = false;
+		// If we're reading the first line of the file, tweet and remember it. Otherwise append it to output
+		if (firstLine == true) {
 
-		var Twitter = require('twitter');
-		var credentials = require("./credentials");
-		var client = new Twitter(credentials);
+		    firstLine = false;
 
-		client.post('statuses/update', {status: line}, function (error, tweet, response) {
-			if (error != null) {
-				console.log(error);
+		    lineToTweet = line;
+
+			var Twitter = require('twitter');
+			var credentials = require("credentials");
+			var client = new Twitter(credentials);
+
+			client.post('statuses/update', {status: lineToTweet}, function (error, tweet, response) {
+				if (error != null) {
+					console.log(error);
+				}
+			});
+
+		}
+		else
+			output += line + '\n';
+	})
+	.on('close', function() {
+
+		// Move the first line to the end of the file (so this can repeat infinitely) and upload to S3
+		output += lineToTweet;
+
+		var params_new = {Bucket: s3_params.bucket, Key: s3_params.key, Body: output, ContentType: 'text/plain'};
+
+		s3.upload(params_new, function(err, data) {
+			if (err) {
+				console.log(err);
+			} 
+			else {
+				console.log(data.ETag);
 			}
 		});
-
-	}
-	else
-		output += line + '\n';
-})
-.on('close', function() {
-	output += lineToTweet;
-	//console.log(output);
-
-	var params_new = {Bucket: s3_params.bucket, Key: s3_params.key, Body: output, ContentType: 'text/plain'};
-
-	s3.upload(params_new, function(err, data) {
-		if (err) {
-			console.log(err);
-			//context.fail("There was an error.");
-		} 
-		else {
-			console.log(data.ETag);
-			//context.succeed(data.ContentType);
-		}
 	});
-});
+}
 
-
-// exports.handler = function (event, context) {
-// 	sendTweet(context.succeed, context.fail)
-// };
+// Set up the exports.handler for AWS Lambda
+exports.handler = function (event, context) {
+	sendTweet(context.succeed, context.fail)
+};

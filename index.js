3e9aca865f457d2f3bbb2dad9dd1c23c7286bc52
Benjamin Haas <benjamin.haas@intersection.com>
2016-06-29 13:46:35 -0400

    code for posting to twitter

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+var Twitter = require('twitter');
 var credentials = require("./credentials");
 var client = new Twitter(credentials);
+
+var sendTweet = function() {
+	client.post('statuses/update', {status: 'test tweet ' + Math.random()}, function (error, tweet, response) {
+		if (error != null) {
+			console.log(error);
+		}
+	});
+};
+
+// client.get('statuses/show/', {id: '747855945355694084'}, function (error, data, response) {
+// 	console.log(error);
+// 	console.log(data);
+// });
+
+exports.handler = function (event, context) {
+	sendTweet(context.succeed, context.fail)
+};
